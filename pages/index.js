95db8bc6e17d835a4a9c778a49c96a54d6e4c5c3
Christos <constantinides.ch@gmail.com>
2020-09-21 20:32:36 +0200

    Fix: data/json return format

--- pages/index.js
+++ pages/index.js
@@ -21,14 +21,7 @@ Home.propTypes = {
 };
 
 export const getServerSideProps = async () => {
-  const req = await fetch(`https://randomuser.me/api/?results=50`, {
-    method: "GET",
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
-      Accept: "application/json",
-    },
-  });
+  const req = await fetch(`https://randomuser.me/api/?results=50`);
   const data = await req.json();
 
   return {
