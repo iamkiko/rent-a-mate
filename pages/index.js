6d7954b4c06abd240d4dabd6bd3c33df68b1d10a
Christos <constantinides.ch@gmail.com>
2020-09-21 20:45:29 +0200

    Fix: Change resultS

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ Home.propTypes = {
 };
 
 export const getServerSideProps = async () => {
-  const req = await fetch(`https://randomuser.me/api/?results=50?format=json`);
+  const req = await fetch(`https://randomuser.me/api/?format=json&results=50`);
   const data = await req.json();
 
   return {
